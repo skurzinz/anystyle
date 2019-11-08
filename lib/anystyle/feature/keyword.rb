d77019a0ff99c771d5be6e6a6b304668f17c8826
skurzinz <37441365+skurzinz@users.noreply.github.com>
2019-11-08 08:42:12 +0100

    more cases for German in keyword.rb

--- lib/anystyle/feature/keyword.rb
+++ lib/anystyle/feature/keyword.rb
@@ -8,7 +8,7 @@ module AnyStyle
         else
           case alpha
           when /^ed(s|itors?|ited?|iteurs?)?$/i,
-               /^(hg|hrsg|herausgeber)$/i,
+               /^(hg(\sv\.)?|hrsg(\sv\.)?|herausgeber)$/i,
                /^(compilador)$/i,
                /編/
             :editor
@@ -19,11 +19,11 @@ module AnyStyle
                /^trad(uction|ucteurs?|uit)?$/i,
                /譯/
             :translator
-          when /^(dissertation|thesis)$/i
+          when /^(dissertation|thesis|diplomarbeit)$/i
             :thesis
           when /^(proceedings|conference|meeting|transactions|communications|seminar|symposi(on|um))/i
             :proceedings
-          when /^(Journal|Zeitschrift|Quarterly|Magazine?|Times|Rev(iew|vue)?|Bulletin|News|Week|Gazett[ea])/
+          when /^(Journal|Zeitschrift|Jahrbuch|Vierteljahresschrift|Quarterly|Magazine?|Times|Rev(iew|vue)?|Bulletin|News|Week|Gazett[ea])/
             :journal
           when /^in$/i, /收入/
             :in
@@ -33,9 +33,9 @@ module AnyStyle
             :etal
           when /^(pp?|pages?|S(eiten?)?|ff?)$/
             :page
-          when /^(vol(ume)?s?|iss(ue)?|n[or]?|number|fasc(icle|icule)?|suppl(ement)?|j(ahrgan)?g|heft)$/i
+          when /^(vol(ume)?s?|iss(ue)?|n[or]?|number|fasc(icle|icule)?|suppl(ement)?|j(ahrgan)?g|heft|b(an?)d)$/i
             :volume
-          when /^(ser(ies?)?|reihe|[ck]oll(e[ck]tion))$/i
+          when /^(ser(ies?)?|(schriften?)reihe|[ck]oll(e[ck]tion)|veröffentlichungen)$/i
             :series
           when /^patent$/i
             :patent
@@ -44,19 +44,20 @@ module AnyStyle
           when /^(edn|edition|expanded|rev(ised)?|p?reprint(ed)?|illustrated)$/i,
             /^editio|aucta$/i
             /^(aufl(age)?|\p{Alpha}*ausg(abe)?)$/i
+            /^((nach)?druck)$/i
             :edition
           when /^(nd|date|spring|s[uo]mmer|autumn|fall|winter|frühling|herbst)$/i,
-               /^(jan(uary?)?|feb(ruary?)?|mar(ch|z)?|apr(il)?|ma[yi]|jun[ei]?)$/,
-               /^(jul[yi]?|aug(ust)?|sep(tember)?|o[ck]t(ober)?|nov(ember)?|de[cz](ember)?)$/i,
+               /^(jan(uary?)?|jän(ner?)?|feb(ruary?)?|m[äa]r(ch|z)?|apr(il)?|ma[yi]|jun[ei]?)$/,
+               /^(jul[yi]?|aug(ust)?|sep(t|tember)?|o[ck]t(ober)?|nov(ember)?|de[cz](ember)?)$/i,
                /年/
             :date
-          when /^(doi|url)/i
+          when /^(doi|url|hdl)/i
             :locator
           when /^(pmid|pmcid)/i
             :pubmed
           when /^(arxiv)/i
             :arxiv
-          when /^(retrieved|retirado|accessed|ab(ruf|gerufen))$/i
+          when /^(retrieved|retirado|accessed|(ab|auf)(ruf|gerufen))$/i
             :accessed
           when /^[ILXVMCD]{2,}$/
             :roman
